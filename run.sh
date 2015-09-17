397ef0d23c1ecf156f136f8359e26c9e8f65a243
Brian Kosick <brian.kosick@readytalk.com>
2015-09-17 15:42:16 -0600

    Initial Commit

---
+++ run.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+echo /bin/sed -i -e "s/INFLUXSERVER/${INFLUX_PORT_8086_TCP_ADDR}/" -e "s/INFLUXSERVERPORT/${INFLUX_PORT_8086_TCP_PORT}/" -e "s/FOXDENDB/${INFLUX_ENV_PRE_CREATE_DB}/" /etc/nginx/nginx.conf 
+/bin/sed -i -e "s/INFLUXSERVER/${INFLUX_PORT_8086_TCP_ADDR}/" -e "s/INFLUXSERVERPORT/${INFLUX_PORT_8086_TCP_PORT}/" -e "s/FOXDENDB/${INFLUX_ENV_PRE_CREATE_DB}/" /etc/nginx/nginx.conf 
+
+nginx
