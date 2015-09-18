e9c9f00f256da1c554aa4998677adc6ee4382128
Brian Kosick <brian.kosick@readytalk.com>
2015-09-18 15:12:21 -0600

    Adding environment dump to run sh

--- run.sh
+++ run.sh
@@ -1,4 +1,7 @@
 #!/bin/bash
+echo "---- Environment ----"
+env
+echo "---------------------"
 echo /bin/sed -i -e "s/INFLUXSERVER/${INFLUX_PORT_8086_TCP_ADDR}/" -e "s/INFLUXSERVERPORT/${INFLUX_PORT_8086_TCP_PORT}/" -e "s/FOXDENDB/${INFLUX_ENV_PRE_CREATE_DB}/" /etc/nginx/nginx.conf 
 /bin/sed -i -e "s/INFLUXSERVER/${INFLUX_PORT_8086_TCP_ADDR}/" -e "s/INFLUXSERVERPORT/${INFLUX_PORT_8086_TCP_PORT}/" -e "s/FOXDENDB/${INFLUX_ENV_PRE_CREATE_DB}/" /etc/nginx/nginx.conf 
 
