2679f102737fa76e35dd6d0c391e7d98dd56c9b5
Brian Kosick <brian.kosick@readytalk.com>
2015-09-18 16:12:44 -0600

    fix ping url for healthcheck

--- run.sh
+++ run.sh
@@ -2,7 +2,7 @@
 echo "---- Environment ----"
 env
 echo "---------------------"
-echo /bin/sed -i -e "s/INFLUXSERVER/${INFLUX_PORT_8086_TCP_ADDR}/" -e "s/INFLUXSERVERPORT/${INFLUX_PORT_8086_TCP_PORT}/" -e "s/FOXDENDB/${INFLUX_ENV_PRE_CREATE_DB}/" /etc/nginx/nginx.conf 
+echo /bin/sed -i -e "s/INFLUXSERVER/${INFLUX_PORT_8086_TCP_ADDR}/g" -e "s/INFLUXSERVERPORT/${INFLUX_PORT_8086_TCP_PORT}/g" -e "s/FOXDENDB/${INFLUX_ENV_PRE_CREATE_DB}/g" /etc/nginx/nginx.conf 
 /bin/sed -i -e "s/INFLUXSERVER/${INFLUX_PORT_8086_TCP_ADDR}/" -e "s/INFLUXSERVERPORT/${INFLUX_PORT_8086_TCP_PORT}/" -e "s/FOXDENDB/${INFLUX_ENV_PRE_CREATE_DB}/" /etc/nginx/nginx.conf 
 
 nginx
