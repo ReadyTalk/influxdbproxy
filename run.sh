32739f8b44c958896f3ef1522a2013b1a273bf79
Brian Kosick <brian.kosick@readytalk.com>
2015-09-21 14:01:35 -0600

    Adding RO_USER and adding query and admin locations

--- run.sh
+++ run.sh
@@ -5,4 +5,10 @@ echo "---------------------"
 echo /bin/sed -i -e "s/INFLUXSERVER/${INFLUX_PORT_8086_TCP_ADDR}/g" -e "s/INFLUXSERVERPORT/${INFLUX_PORT_8086_TCP_PORT}/g" -e "s/FOXDENDB/${INFLUX_ENV_PRE_CREATE_DB}/g" /etc/nginx/nginx.conf 
 /bin/sed -i -e "s/INFLUXSERVER/${INFLUX_PORT_8086_TCP_ADDR}/" -e "s/INFLUXSERVERPORT/${INFLUX_PORT_8086_TCP_PORT}/" -e "s/FOXDENDB/${INFLUX_ENV_PRE_CREATE_DB}/" /etc/nginx/nginx.conf 
 
+echo /bin/sed -i -e "s/INFLUXADMINSERVER/${INFLUX_PORT_8083_TCP_ADDR}/" -e "s/INFLUXADMINSERVERPORT/${INFLUX_PORT_8083_TCP_PORT}/" /etc/nginx/nginx.conf 
+/bin/sed -i -e "s/INFLUXADMINSERVER/${INFLUX_PORT_8083_TCP_ADDR}/" -e "s/INFLUXADMINSERVERPORT/${INFLUX_PORT_8083_TCP_PORT}/" /etc/nginx/nginx.conf 
+
+echo /bin/sed -i -e "s/WOUSER/${WO_USER}/" -e "s/WOPASS/${WO_PASS}/" /etc/nginx/nginx.conf 
+/bin/sed -i -e "s/WOUSER/${WO_USER}/" -e "s/WOPASS/${WO_PASS}/" /etc/nginx/nginx.conf 
+
 nginx
